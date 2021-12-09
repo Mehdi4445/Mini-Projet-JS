84ec6c1234e168ff7c11660553a7e0d21becb1fe
Mehdi <mehdi.medimegh@isamm.u-manouba.tn>
2021-12-09 22:02:25 +0100

    half way

--- js/main.js
+++ js/main.js
@@ -1,33 +1,57 @@
-
-(function ($) {
-
-
-    $(window).on('load', function () {
-        $(".loader").fadeOut();
-        $("#preloder").delay(200).fadeOut("slow");
-    });
-    
+$(window).on('load', function () {
+    $(".loader").fadeOut();
+    $("#preloder").delay(200).fadeOut("slow");
+});
 
 
-    $('.set-bg').each(function () {
-        var bg = $(this).data('setbg');
-        $(this).css('background-image', 'url(' + bg + ')');
-    });
+
+$('.set-bg').each(function () {
+    var bg = $(this).data('setbg');
+    $(this).css('background-image', 'url(' + bg + ')');
+});
 
 
-    var hero_s = $(".hs-slider");
-    hero_s.owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: true,
-        items: 1,
-        dots: false,
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
-        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-        smartSpeed: 1200,
-        autoHeight: false,
-        autoplay: false
-    });
+var hero_s = $(".hs-slider");
+hero_s.owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    items: 1,
+    dots: false,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+    smartSpeed: 1200,
+    autoHeight: false,
+    autoplay: false
+});
 
-})(jQuery);
+function getGoal() {
+    var x = document.getElementById("goal").value;
+
+    if (x == 0) {
+
+        document.getElementById("choice_1").style.display = "none"
+        document.getElementById("choice_2").style.display = "none"
+
+    } else if (x == 1) {
+
+        document.getElementById("choice_1").style.display = "block"
+        document.getElementById("choice_2").style.display = "none"
+
+    } else {
+
+        document.getElementById("choice_1").style.display = "none"
+        document.getElementById("choice_2").style.display = "block"
+    }
+}
+
+function addAllergie(where) {
+    var main = document.getElementById("allergies");
+    var cntr = (main.datacntr || 0) + 1;
+    main.datacntr = cntr;
+
+    var clone = main.cloneNode(true);
+    clone.id = "section" + cntr;
+    where.parentNode.insertBefore(clone, where);
+}
